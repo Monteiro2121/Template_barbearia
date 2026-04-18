82722f99f27c49f7cacc40505539d0dca7ee21fe
Monteiro2121 <lipemontefarias@gmail.com>
2026-04-18 17:06:08 -0300

    fix: padronizando nomes de arquivos para o deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Paginainicial from "./paginainicial.jsx";
-import Servicos from "./Servicos.jsx"; 
-import Endereco from "./Endereco.jsx";
+import Servicos from "./Servicos";
+import Endereco from "./Endereco";
+import Paginainicial from "./Paginainicial";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
